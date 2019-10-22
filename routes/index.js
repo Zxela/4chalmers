0a32dff47db6c7d1ff3821b46b6c4038d3e06be8
Alex Zvaniga <alex.zvaniga@icloud.com>
2019-10-21 20:47:36 -0400

    added routes for each example

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,29 @@
 
 module.exports = function (app) {
   // Register Routes
-  
-  // Home Page
-  get('/', function(req, res, next) {
-    res.render('index', { title: 'Sample API Server' });
-  })
 
-  // API Routes
+  // Home Page
+  app.route('/')
+    .get(function (req, res, next) {
+      res.render('index', {});
+    })
+
+  /**
+   * API Routes
+   */
   
   app.route('/api')
-    .get(() => {console.log("hello")})
+    .get(() => {
+      console.log("hello")
+    })
+  
+  // CSV Example
+  app.route('/api/Toronto')
+  // JSON Example
+  app.route('/api/Barrie')
+  // Mock API Example
+  app.route('/api/London')
+  // SQL Example
+  app.route('/api/Mississauga')
+
 }
